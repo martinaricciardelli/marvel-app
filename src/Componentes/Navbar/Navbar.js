0fc2fed2f8f9b715946eb4ce5c68a5028759ed5d
martinaricciardelli <martina.ricciardelli@gmail.com>
2021-11-17 10:28:33 -0300

    logo y efectos en navbar

--- src/Componentes/Navbar/Navbar.js
+++ src/Componentes/Navbar/Navbar.js
@@ -1,18 +1,16 @@
 import React from 'react'
-//import CartWidget from '../CartWidget/CartWidget'
 import '../Navbar/Navbar.css'
-import '../CartWidget/CartWidget'
-import CartWidget from '../CartWidget/CartWidget'
+import CartWidget from '../CartWidget/CartWidget';
+import logo from '../../assets/img/marvel-logo.png'
 //import { NavLink } from 'react-router-dom'
 
 
-// ARREGLAR LOGOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg" style= {{"background-color": "red"}}>
+        <nav className="navbar navbar-expand-lg" style= {{"background-color": "#e60000"}}>
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img src="../Navbar/marvel-logo"/> 
+                <img src={logo} alt="logo-marvel" style= {{"width": "15vh"}}/> 
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
